9eca1fce8d8b5e7f33ae9367a14152bbd1e84de7
GuigzLab <guillaume.dufau.pro@gmail.com>
2020-04-03 01:24:58 +0200

    v1.0.1
    build added

---
+++ build/precache-manifest.54ed6d39acf59fae3eb1662d31161013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f26d29edb237b32f2fd7055ab69fa8e",
+    "url": "/onzer/index.html"
+  },
+  {
+    "revision": "73b363eadffc01028885",
+    "url": "/onzer/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "227602ff7ad248e3548c",
+    "url": "/onzer/static/js/2.83003c46.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/onzer/static/js/2.83003c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b363eadffc01028885",
+    "url": "/onzer/static/js/main.8e2b1b30.chunk.js"
+  },
+  {
+    "revision": "9b3d5c7fc04620d42382",
+    "url": "/onzer/static/js/runtime-main.86a161b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/onzer/static/media/logo.5d5d9eef.svg"
+  }
+]);
